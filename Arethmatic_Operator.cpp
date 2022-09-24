c862a0c1c97a20e94756979036c65ef48d7bc2a7
ARIF <arifmd0802@gmail.com>
2022-09-24 22:48:43 +0530

    initial commit

---
+++ Arethmatic_Operator.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+int a = 2/5;
+cout<< a << endl;
+
+int b = 2.0/5;
+ double c = 2.0/5;
+ float d = 2.0/5;
+ cout << c << endl;
+ cout<< d << endl;
+cout <<b << endl;
+cout << 2.0/5 <<endl;
+
+
+
+
+}
