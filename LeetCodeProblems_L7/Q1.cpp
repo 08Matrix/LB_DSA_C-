911bded3e3e97eda1ea363afe9e0ddb6c961031f
ARIF <arifmd0802@gmail.com>
2022-09-26 19:52:33 +0530

    Q1

--- LeetCodeProblems_L7/Q1.cpp
+++ LeetCodeProblems_L7/Q1.cpp
@@ -3,5 +3,25 @@ using namespace std;
 
 int main()
 {
+    int x = 0;
+    int ans = 0;
+    cin>> x;
+    int INT_MAX = 0;
+    int INT_MIN = 0;
+    while( x != 0)
+    {
+        int digit = x % 10;
+
+        if ((ans > INT_MAX/10 ) || (ans < INT_MIN/10 )  )
+        {
+            return 0;
+        }
+
+        ans = (ans * 10 ) + digit ;
+        x = x / 10; 
+    }
+        
+        return ans;
     
+
 }
